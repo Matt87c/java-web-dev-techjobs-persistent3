67007e5e858bf367fb216198d6882491673f0b68
Matthew Clockel <35537679+Matt87c@users.noreply.github.com>
2020-09-03 21:06:13 -0400

    Add files via upload

---
+++ java-web-dev-techjobs-persistent2/java-web-dev-techjobs-persistent-master/src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -0,0 +1,37 @@
+package org.launchcode.javawebdevtechjobspersistent.models;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Skill extends AbstractEntity {
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    @NotBlank(message = "Please enter your skills.")
+    @Size(min = 1, max = 500)
+    private String description;
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public Skill(){}
+
+    @Override
+    public Object getEmployer() {
+        return null;
+    }
+}
+
