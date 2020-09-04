edb44fd339b1ffc0eb3dde105a14d1093e19ab0c
Matthew Clockel <35537679+Matt87c@users.noreply.github.com>
2020-09-03 22:23:11 -0400

    Update queries.sql

--- java-web-dev-techjobs-persistent2/java-web-dev-techjobs-persistent-master/queries.sql
+++ java-web-dev-techjobs-persistent2/java-web-dev-techjobs-persistent-master/queries.sql
@@ -6,8 +6,7 @@ WHERE location = 'St Louis City';
 ## Part 3: Test it with SQL
 DROP TABLE job;
 ## Part 4: Test it with SQL
-SELECT name, description
+SELECT DISTINCT name, description
 FROM skill
-INNER JOIN job ON skill
-WHERE skill IS NOT NULL
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
 ORDER BY name;
